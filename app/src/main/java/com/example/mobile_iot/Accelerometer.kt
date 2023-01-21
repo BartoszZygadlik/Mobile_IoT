b6c49faf488b99f1f5c0934333919bc5f7932d79
bzygadlik <bartosz.zygadlik@gmail.com>
2023-01-21 23:51:02 +0100

    Working static chart

--- app/src/main/java/com/example/mobile_iot/Accelerometer.kt
+++ app/src/main/java/com/example/mobile_iot/Accelerometer.kt
@@ -1,6 +1,7 @@
 package com.example.mobile_iot
 
 
+import android.graphics.Color
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -8,8 +9,17 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.androidplot.xy.LineAndPointFormatter
+import com.androidplot.xy.PanZoom
+import com.androidplot.xy.SimpleXYSeries
+import com.androidplot.xy.XYGraphWidget
+import com.androidplot.xy.XYSeries
 
 import com.example.mobile_iot.databinding.ActivityAccelerometerBinding
+import java.text.FieldPosition
+import java.text.Format
+import java.text.ParsePosition
+import java.util.*
 
 class Accelerometer : AppCompatActivity(), SensorEventListener {
     private lateinit var binding: ActivityAccelerometerBinding
@@ -23,6 +33,29 @@ class Accelerometer : AppCompatActivity(), SensorEventListener {
         setContentView(binding.root)
 
         setUpSensor()
+
+        var domainLabels = arrayOf<Number>(1,2,3,4,5,6,7,8,9,10)
+        //var series1Number = arrayOf<Number>(50,2,3,14,-20,50,2,3,14,-20)
+
+        val series1 : XYSeries = SimpleXYSeries(Arrays.asList(* AccVm.series1Number),SimpleXYSeries.ArrayFormat.Y_VALS_ONLY,
+        "Series 1 ") //dane
+        val series1Format = LineAndPointFormatter(Color.BLUE,Color.RED,null,null)
+
+        binding.AccPlot.addSeries(series1,series1Format)
+        binding.AccPlot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format(){
+            override fun format(p0: Any?, p1: StringBuffer?, p2: FieldPosition?): StringBuffer {
+                val i = Math.round((p0 as Number).toFloat())
+
+                return p1!!.append(domainLabels[i])
+
+            }
+
+            override fun parseObject(p0: String?, p1: ParsePosition?): Any? {
+                return null
+            }
+
+        }
+        PanZoom.attach(binding.AccPlot)
     }
 
     private fun setUpSensor() {
@@ -41,6 +74,9 @@ class Accelerometer : AppCompatActivity(), SensorEventListener {
             val xAxis = event.values[0]// m/s^2
             val yAxis = event.values[1]
             val zAxis = event.values[2]
+
+            AccVm.series1Number.plus(event.values[0])
+            AccVm.series1Number.toMutableList().removeAt(0)
 
             binding.xAxisTextView.text = xAxis.toString()
             binding.yAxisTextView.text = yAxis.toString()
--- app/src/main/java/com/example/mobile_iot/AccelerometerViewModel.kt
+++ app/src/main/java/com/example/mobile_iot/AccelerometerViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.ViewModel
 class AccelerometerViewModel: ViewModel() {
     val AccSapling = 200000 //microseconds 200000 micro = 20 mili  m/s^2
     val AccUnit = "m/s^2"
+    var series1Number = arrayOf<Number>(-15,2,3,14,-20,50,2,3,14,-20)
+    var domainLabels = arrayOf<Number>(1,2,3,4,5,6,7,8,9,10)
     //array x
     //array y
     //array z
